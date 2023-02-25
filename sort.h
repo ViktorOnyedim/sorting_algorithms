2870e9496ec53d548edf325e64374386ed50ff15
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-25 10:35:06 +0000

    Modified sort.h

--- sort.h
+++ sort.h
@@ -33,5 +33,8 @@ void quick_sort_recursive(int *array, size_t size, int low, int high);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void TDMerge(size_t start, size_t middle, size_t end, int *dest, int *source);
+void TDSplitMerge(size_t start, size_t end, int *array, int *copy);
 
 #endif /* SORT_H */
