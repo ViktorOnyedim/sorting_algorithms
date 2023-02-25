2745acb0d45207df4210c01958311c17592f3b88
ViktorOnyedim <viktoronyedim@gmail.com>
2023-02-25 09:26:20 +0000

    Updated sort.h

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ void quick_sort_recursive(int *array, size_t size, int low, int high);
 /* end of quick_sort */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif /* SORT_H */
